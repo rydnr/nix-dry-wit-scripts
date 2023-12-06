bd4ec046ba76bc0f8991532ee5c3ce56dae8aabc
rydnr <github@acm-sl.org>
2023-12-06 15:30:10 +0100

    Initial scripts to update flakes

---
+++ update-latest-inputs-nix-flake.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env dry-wit
+# Copyright 2023-today Automated Computing Machinery S.L.
+# Distributed under the terms of the GNU General Public License v3
+
+DW.import file;
+DW.import nix-flake;
+DW.import git;
+
+# fun: main
+# api: public
+# txt: Main logic. Gets called by dry-wit.
+# txt: Returns 0/TRUE always, but may exit due to errors.
+# use: main
+function main() {
+  local _flakeNix="${FLAKE_NIX_FILE}";
+  if isEmpty "${_flakeNix}"; then
+    _flakeNix="${PWD}/flake.nix";
+    if ! fileExists "${_flakeNix}"; then
+      _flakeNix="${PWD}/nix/flake.nix";
+    fi
+    if ! fileExists "${_flakeNix}"; then
+      exitWithErrorCode FLAKE_NIX_FILE_DOES_NOT_EXIST "${PWD}";
+    fi
+  fi
+
+  local _flakeLock="${FLAKE_LOCK_FILE}";
+  if isEmpty "${_flakeLock}"; then
+    _flakeLock="$(dirname "${_flakeLock}")/$(basename ${_flakeNix} .nix).lock";
+    if ! fileExists "${_flakeLock}"; then
+      exitWithErrorCode FLAKE_LOCK_FILE_DOES_NOT_EXIST "${PWD}";
+    fi
+  fi
+
+  local _inputs;
+  logDebug -n "Extracting inputs from ${_flakeNix}";
+  if extract_inputs_in_flake_lock "${_flakeLock}"; then
+    _inputs="${RESULT}";
+    logDebugResult SUCCESS "done";
+  else
+    logDebugResult FAILURE "error";
+    exitWithErrorCode CANNOT_EXTRACT_INPUTS_FROM_FLAKE_LOCK "${_flakeLock}";
+  fi
+
+  local _origIFS="${IFS}";
+  IFS="${DWIFS}";
+  for _input in ${_inputs}; do
+    IFS=${_origIFS};
+    local _name="$(echo "${_input}" | cut -d ':' -f 1)";
+    local _owner="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 1)";
+    local _repo="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 2)";
+    local _tag="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 3)";
+    if retrieveLatestRemoteTagInGithub "${_owner}" "${_repo}" "${GITHUB_TOKEN}"; then
+      local _latest_tag="${RESULT}";
+      if [[ "${_tag}" != "${_latest_tag}" ]]; then
+        logInfo -n "Updating ${_owner}/${_repo} from ${_tag} to ${_latest_tag}";
+        if update_urls_in_flake "github:${_owner}/${_repo}/${_tag}" "github:${_owner}/${_repo}/${_latest_tag}" "${_flakeNix}"; then
+          logInfoResult SUCCESS "done";
+        else
+          logInfoResult FAILURE "failed";
+          exitWithErrorCode CANNOT_UPDATE_FLAKE_INPUT_IN_FLAKE "${_input}";
+        fi
+      fi
+    fi
+  done
+  IFS=${_origIFS};
+}
+
+## Script metadata and CLI settings.
+setScriptDescription "Updates the flake inputs to its latest versions";
+setScriptLicenseSummary "Distributed under the terms of the GNU General Public License v3";
+setScriptCopyright "Copyleft 2023-today Automated Computing Machinery S.L.";
+
+addCommandLineFlag "flakeNix" "f" "The flake.nix file" OPTIONAL EXPECTS_ARGUMENT;
+addCommandLineFlag "flakeLock" "l" "The flake.nix file" OPTIONAL EXPECTS_ARGUMENT;
+addCommandLineFlag "githubToken" "t" "The github token" OPTIONAL EXPECTS_ARGUMENT;
+
+checkReq jq;
+checkReq sed;
+checkReq grep;
+
+addError FLAKE_NIX_FILE_DOES_NOT_EXIST "flake.nix not specified and not found in ";
+addError FLAKE_LOCK_FILE_DOES_NOT_EXIST "flake.nix not specified and not found in ";
+addError CANNOT_RETRIEVE_LATEST_VERSION_OF_REPO "Cannot retrieve the latest version of repo ";
+addError CANNOT_EXTRACT_INPUTS_FROM_FLAKE_LOCK "Cannot extract the inputs from given flake.lock file";
+addError CANNOT_EXTRACT_ORG_FROM_FLAKE_INPUT "Cannot extract the 'org' value from ";
+addError CANNOT_EXTRACT_REPO_FROM_FLAKE_INPUT "Cannot extract the 'repo' value from ";
+addError CANNOT_UPDATE_FLAKE_INPUT_IN_FLAKE "Cannot update the version of the input ";
+
+function dw_parse_flake_nix_cli_flag() {
+  export FLAKE_NIX="${1}";
+}
+function dw_parse_flake_lock_cli_flag() {
+  export FLAKE_LOCK="${1}";
+}
+function dw_parse_github_token_cli_flag() {
+  export GITHUB_TOKEN="${1}";
+}
+# vim: syntax=sh ts=2 sw=2 sts=4 sr noet
---
+++ update-sha256-nix-flake.sh
@@ -0,0 +1,129 @@
+#!/usr/bin/env dry-wit
+# Copyright 2023-today Automated Computing Machinery S.L.
+# Distributed under the terms of the GNU General Public License v3
+
+DW.import file;
+DW.import nix-flake;
+DW.import git;
+
+# fun: main
+# api: public
+# txt: Main logic. Gets called by dry-wit.
+# txt: Returns 0/TRUE always, but may exit due to errors.
+# use: main
+function main() {
+  local _org;
+
+  local _flakeFile="${FLAKE_FILE}";
+  if isEmpty "${_flakeFile}"; then
+    _flakeFile="${PWD}/flake.nix";
+    if ! fileExists "${_flakeFile}"; then
+      _flakeFile="${PWD}/nix/flake.nix";
+    fi
+    if ! fileExists "${_flakeFile}"; then
+      exitWithErrorCode FLAKE_FILE_DOES_NOT_EXIST "${PWD}";
+    fi
+  fi
+
+  logDebug -n "Extracting org from ${_flakeFile}";
+  if extract_org "${_flakeFile}"; then
+    _org="${RESULT}";
+    logDebugResult SUCCESS "${_org}";
+  else
+    logDebugResult FAILURE "error";
+    exitWithErrorCode CANNOT_EXTRACT_ORG_FROM_FLAKE "${_flakeFile}";
+  fi
+
+  local _repo;
+  logDebug -n "Extracting repo from ${_flakeFile}";
+  if extract_repo "${_flakeFile}"; then
+    _repo="${RESULT}";
+    logDebugResult SUCCESS "${_repo}";
+  else
+    logDebugResult FAILURE "error";
+    exitWithErrorCode CANNOT_EXTRACT_REPO_FROM_FLAKE "${_flakeFile}";
+  fi
+
+  local _projectVersion="${PROJECT_VERSION}";
+  if isEmpty "${_projectVersion}"; then
+    logDebug -n "Retrieving latest tag of ${_org}/${_repo}";
+    if retrieveLatestRemoteTagInGithub "${_org}" "${_repo}"; then
+      _projectVersion="${RESULT}";
+      logDebugResult SUCCESS "${_projectVersion}";
+    else
+      logDebugResult FAILURE "error";
+      exitWithErrorCode CANNOT_RETRIEVE_LATEST_VERSION_OF_REPO "${_repo}";
+    fi
+  fi
+
+  local -i _updateSha256=${FALSE};
+
+  if fileContains "${_flakeFile}" "sha256 ="; then
+    _updateSha256=${TRUE};
+  fi
+
+  logDebug -n "Updating version in ${_flakeFile} to ${_projectVersion}";
+  if update_version_in_flake "${_flakeFile}" "${_projectVersion}"; then
+    logDebugResult SUCCESS "done";
+  else
+    logDebugResult FAILURE "error";
+    exitWithErrorCode CANNOT_UPDATE_VERSION_IN_FLAKE "${_flakeFile} ${_projectVersion}";
+  fi
+
+  if isTrue ${_updateSha256}; then
+    local _sha256;
+    local _url="https://github.com/${_org}/${_repo}";
+    logDebug -n "Fetching sha256 of ${_url}, rev ${_projectVersion}"
+    if fetch_sha256 "${_url}" "${_projectVersion}"; then
+      _sha256="${RESULT}";
+      logDebugResult SUCCESS "${_sha256}";
+    else
+      logDebugResult FAILURE "error";
+      exitWithErrorCode CANNOT_FETCH_SHA256_FROM_URL "${_url} ${_projectVersion}";
+    fi
+
+    logDebug -n "Updating sha256 in ${_flakeFile}";
+    if update_sha256_in_flake "${_flakeFile}" "${_sha256}"; then
+      logDebugResult SUCCESS "done";
+    else
+      logDebugResult FAILURE "error";
+      exitWithErrorCode CANNOT_UPDATE_SHA256_IN_FLAKE "${_flakeFile} ${_sha256}";
+    fi
+  fi
+
+  if isTrue ${_updateSha256}; then
+    logInfo "Updated version and sha256 in ${_flakeFile}"
+  else
+    logInfo "Updated version in ${_flakeFile}"
+  fi
+}
+
+## Script metadata and CLI settings.
+setScriptDescription "Updates the version and sha256 hash of a PythonEDA-specific Nix flake";
+setScriptLicenseSummary "Distributed under the terms of the GNU General Public License v3";
+setScriptCopyright "Copyleft 2023-today Automated Computing Machinery S.L.";
+
+addCommandLineFlag "version" "V" "The version" OPTIONAL EXPECTS_ARGUMENT;
+addCommandLineFlag "flake" "f" "The Nix flake" OPTIONAL EXPECTS_ARGUMENT;
+
+checkReq nix-prefetch-git;
+checkReq jq;
+checkReq sed;
+checkReq grep;
+
+addError FLAKE_FILE_DOES_NOT_EXIST "Flake file not specified and not found in ";
+addError CANNOT_RETRIEVE_LATEST_VERSION_OF_REPO "Cannot retrieve the latest version of repo ";
+addError CANNOT_EXTRACT_ORG_FROM_FLAKE "Cannot extract the 'org' value from ";
+addError CANNOT_EXTRACT_REPO_FROM_FLAKE "Cannot extract the 'repo' value from ";
+addError CANNOT_FETCH_SHA256_FROM_URL "Cannot fetch the sha256 hash from ";
+addError CANNOT_UPDATE_VERSION_IN_FLAKE "Cannot update the 'version' value in ";
+addError CANNOT_UPDATE_SHA256_IN_FLAKE "Cannot update the 'sha256' value in ";
+
+function dw_parse_version_cli_flag() {
+  export PROJECT_VERSION="${1}";
+}
+
+function dw_parse_flake_cli_flag() {
+  export FLAKE_FILE="${1}";
+}
+# vim: syntax=sh ts=2 sw=2 sts=4 sr noet
