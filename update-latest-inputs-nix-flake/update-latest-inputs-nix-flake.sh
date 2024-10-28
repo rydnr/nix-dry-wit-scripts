1f19842038a65e85ea9d9e6c9b31e04c77a52a18
rydnr <github@acm-sl.org>
2024-10-28 19:07:05 +0100

    Minor fix

--- update-latest-inputs-nix-flake/update-latest-inputs-nix-flake.sh
+++ update-latest-inputs-nix-flake/update-latest-inputs-nix-flake.sh
@@ -2,10 +2,10 @@
 # Copyright 2023-today Automated Computing Machinery S.L.
 # Distributed under the terms of the GNU General Public License v3
 
-DW.import file;
-DW.import nix-flake;
-DW.import git;
-DW.import github;
+DW.import file
+DW.import nix-flake
+DW.import git
+DW.import github
 
 # fun: main
 # api: public
@@ -13,135 +13,135 @@ DW.import github;
 # txt: Returns 0/TRUE if some inputs get updated; 1/FALSE otherwise, and other values in case of errors.
 # use: if main; then echo "Inputs updated"; fi
 function main() {
-  local _flakeNix="${FLAKE_NIX_FILE}";
+  local _flakeNix="${FLAKE_NIX_FILE}"
 
-  local -i _rescode=${FALSE};
+  local -i _rescode=${FALSE}
 
   if isEmpty "${_flakeNix}"; then
-    _flakeNix="${PWD}/flake.nix";
+    _flakeNix="${PWD}/flake.nix"
     if ! fileExists "${_flakeNix}"; then
-      _flakeNix="${PWD}/nix/flake.nix";
+      _flakeNix="${PWD}/nix/flake.nix"
     fi
     if ! fileExists "${_flakeNix}"; then
-      exitWithErrorCode FLAKE_NIX_FILE_DOES_NOT_EXIST "${PWD}";
+      exitWithErrorCode FLAKE_NIX_FILE_DOES_NOT_EXIST "${PWD}"
     fi
   fi
 
-  local _folder;
-  _folder="$(command realpath "$(command dirname ${_flakeNix})")";
+  local _folder
+  _folder="$(command realpath "$(command dirname ${_flakeNix})")"
 
-  local _flakeLock="${FLAKE_LOCK_FILE}";
+  local _flakeLock="${FLAKE_LOCK_FILE}"
   if isEmpty "${_flakeLock}"; then
-    _flakeLock="$(command dirname "${_flakeLock}")/$(command basename ${_flakeNix} .nix).lock";
+    _flakeLock="$(command dirname "${_flakeLock}")/$(command basename ${_flakeNix} .nix).lock"
     if ! fileExists "${_flakeLock}"; then
-      exitWithErrorCode FLAKE_LOCK_FILE_DOES_NOT_EXIST "${_folder}/flake.lock";
+      exitWithErrorCode FLAKE_LOCK_FILE_DOES_NOT_EXIST "${_flakeLock}"
     fi
   fi
 
-  local _error;
-  logDebug -n "Updating $(command dirname "${_folder}")/flake.lock";
+  local _error
+  logDebug -n "Updating $(command realpath "${_flakeLock}")"
   if updateFlakeLock "${_flakeNix}" "${GITHUB_TOKEN}"; then
-    logDebugResult SUCCESS "done";
+    logDebugResult SUCCESS "done"
   else
-    _error="${ERROR}";
-    logDebugResult FAILURE "failed";
+    _error="${ERROR}"
+    logDebugResult FAILURE "failed"
     if isNotEmpty "${_error}"; then
-      logDebug "${_error}";
+      logDebug "${_error}"
     fi
-    exitWithErrorCode CANNOT_UPDATE_FLAKE_LOCK "${_folder}/flake.lock";
+    exitWithErrorCode CANNOT_UPDATE_FLAKE_LOCK "${_flakeLock}"
   fi
 
-  local _inputs;
-  logDebug -n "Extracting inputs from ${_flakeNix}";
+  local _inputs
+  logDebug -n "Extracting inputs from ${_flakeNix}"
   if extractInputsFromFlakeLock "${_flakeLock}"; then
-    _inputs="${RESULT}";
-    logDebugResult SUCCESS "done";
+    _inputs="${RESULT}"
+    logDebugResult SUCCESS "done"
   else
-    logDebugResult FAILURE "error";
-    exitWithErrorCode CANNOT_EXTRACT_INPUTS_FROM_FLAKE_LOCK "${_flakeLock}";
+    logDebugResult FAILURE "error"
+    exitWithErrorCode CANNOT_EXTRACT_INPUTS_FROM_FLAKE_LOCK "${_flakeLock}"
   fi
 
-  local _name;
-  local _owner;
-  local _repo;
-  local _tag;
-  local _latestTag;
-  local _origIFS="${IFS}";
-  IFS="${DWIFS}";
+  local _name
+  local _owner
+  local _repo
+  local _tag
+  local _latestTag
+  local _origIFS="${IFS}"
+  IFS="${DWIFS}"
   for _input in ${_inputs}; do
-    IFS=${_origIFS};
-    _name="$(echo "${_input}" | cut -d ':' -f 1)";
-    _owner="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 1)";
-    _repo="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 2)";
-    _tag="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 3)";
-    logDebug -n "github:${_owner}/${_repo}";
-    _latestTag="";
+    IFS=${_origIFS}
+    _name="$(echo "${_input}" | cut -d ':' -f 1)"
+    _owner="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 1)"
+    _repo="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 2)"
+    _tag="$(echo "${_input}" | cut -d ':' -f 2 | cut -d '/' -f 3)"
+    logDebug -n "github:${_owner}/${_repo}"
+    _latestTag=""
     if areEqual "${_owner}" "NixOS" && areEqual "${_repo}" "nixpkgs"; then
       if retrieveLatestStableNixpkgsTag "${GITHUB_TOKEN}"; then
-        _latestTag="${RESULT}";
+        _latestTag="${RESULT}"
       fi
     elif retrieveLatestRemoteTagInGithub "${_owner}" "${_repo}" "${GITHUB_TOKEN}"; then
-      _latestTag="${RESULT}";
+      _latestTag="${RESULT}"
     fi
     if isEmpty "${_latestTag}"; then
-      _error="${ERROR}";
-      logDebugResult NEUTRAL "skipped";
+      _error="${ERROR}"
+      logDebugResult NEUTRAL "skipped"
       if isNotEmpty "${_error}"; then
-        logDebug "${_error}";
+        logDebug "${_error}"
       fi
     else
-      logDebugResult SUCCESS "${_latestTag}";
+      logDebugResult SUCCESS "${_latestTag}"
       if ! areEqual "${_tag}" "${_latestTag}"; then
-        _rescode=${TRUE};
-        logInfo -n "$(command dirname ${_folder}):${_owner}/${_repo}:${_tag}";
+        _rescode=${TRUE}
+        logInfo -n "$(command dirname ${_folder}):${_owner}/${_repo}:${_tag}"
         if updateInputsInFlakeNix "github:${_owner}/${_repo}/${_tag}" "github:${_owner}/${_repo}/${_latestTag}" "${_flakeNix}"; then
-          logInfoResult SUCCESS "${_latestTag}";
-          logDebug -n "Updating $(command realpath "${_flakeLock}")";
+          logInfoResult SUCCESS "${_latestTag}"
+          logDebug -n "Updating $(command realpath "${_flakeLock}")"
           if updateFlakeLock "${_flakeNix}" "${GITHUB_TOKEN}"; then
-            logInfoResult SUCCESS "done";
+            logInfoResult SUCCESS "done"
           else
-            _error="${ERROR}";
-            logInfoResult FAILURE "failed";
+            _error="${ERROR}"
+            logInfoResult FAILURE "failed"
             if isNotEmpty "${_error}"; then
-              logDebug "${_error}";
+              logDebug "${_error}"
             fi
-            exitWithErrorCode CANNOT_UPDATE_FLAKE_LOCK "${_folder}/flake.lock";
+            exitWithErrorCode CANNOT_UPDATE_FLAKE_LOCK "${_flakeLock}"
           fi
         else
-          local _error="${ERROR}";
-          logInfoResult FAILURE "failed";
+          local _error="${ERROR}"
+          logInfoResult FAILURE "failed"
           if isNotEmpty "${_error}"; then
-            logDebug "${_error}";
+            logDebug "${_error}"
           fi
-          exitWithErrorCode CANNOT_UPDATE_FLAKE_INPUT_IN_FLAKE "${_input}";
+          exitWithErrorCode CANNOT_UPDATE_FLAKE_INPUT_IN_FLAKE "${_input}"
         fi
       fi
     fi
   done
-  IFS=${_origIFS};
+  IFS=${_origIFS}
 
-  return ${_rescode};
+  return ${_rescode}
 }
 
 ## Script metadata and CLI settings.
-setScriptDescription "Updates the flake inputs to their latest versions";
-setScriptLicenseSummary "Distributed under the terms of the GNU General Public License v3";
-setScriptCopyright "Copyleft 2023-today Automated Computing Machinery S.L.";
+setScriptDescription "Updates the flake inputs to their latest versions"
+setScriptLicenseSummary "Distributed under the terms of the GNU General Public License v3"
+setScriptCopyright "Copyleft 2023-today Automated Computing Machinery S.L."
 
-addCommandLineFlag "flakeNix" "f" "The flake.nix file" OPTIONAL EXPECTS_ARGUMENT;
-addCommandLineFlag "flakeLock" "l" "The flake.lock file" OPTIONAL EXPECTS_ARGUMENT;
-addCommandLineFlag "githubToken" "t" "The github token" OPTIONAL EXPECTS_ARGUMENT;
+addCommandLineFlag "flakeNix" "f" "The flake.nix file" OPTIONAL EXPECTS_ARGUMENT
+addCommandLineFlag "flakeLock" "l" "The flake.lock file" OPTIONAL EXPECTS_ARGUMENT
+addCommandLineFlag "githubToken" "t" "The github token" OPTIONAL EXPECTS_ARGUMENT
 
-checkReq jq;
-checkReq sed;
-checkReq grep;
+checkReq jq
+checkReq sed
+checkReq grep
 
-addError FLAKE_NIX_FILE_DOES_NOT_EXIST "flake.nix not specified and not found in";
-addError FLAKE_LOCK_FILE_DOES_NOT_EXIST "flake.lock not specified and not found in";
-addError CANNOT_RETRIEVE_LATEST_VERSION_OF_REPO "Cannot retrieve the latest version of repo";
-addError CANNOT_EXTRACT_INPUTS_FROM_FLAKE_LOCK "Cannot extract the inputs from given flake.lock file";
-addError CANNOT_EXTRACT_ORG_FROM_FLAKE_INPUT "Cannot extract the 'org' value from";
-addError CANNOT_EXTRACT_REPO_FROM_FLAKE_INPUT "Cannot extract the 'repo' value from";
-addError CANNOT_UPDATE_FLAKE_INPUT_IN_FLAKE "Cannot update the version of the input";
-addError CANNOT_UPDATE_FLAKE_LOCK "Cannot update";
+addError FLAKE_NIX_FILE_DOES_NOT_EXIST "flake.nix not specified and not found in"
+addError FLAKE_LOCK_FILE_DOES_NOT_EXIST "flake.lock not specified and not found in"
+addError CANNOT_RETRIEVE_LATEST_VERSION_OF_REPO "Cannot retrieve the latest version of repo"
+addError CANNOT_EXTRACT_INPUTS_FROM_FLAKE_LOCK "Cannot extract the inputs from given flake.lock file"
+addError CANNOT_EXTRACT_ORG_FROM_FLAKE_INPUT "Cannot extract the 'org' value from"
+addError CANNOT_EXTRACT_REPO_FROM_FLAKE_INPUT "Cannot extract the 'repo' value from"
+addError CANNOT_UPDATE_FLAKE_INPUT_IN_FLAKE "Cannot update the version of the input"
+addError CANNOT_UPDATE_FLAKE_LOCK "Cannot update"
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
